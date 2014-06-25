2d499d0a9e28996956f50147d3779d617b819075
jonarnaldo <jonarnaldo@gmail.com>
2014-06-25 16:05:15 -0700

    Added Phrases

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 Lang = {
-	English: ["welcome","hello","I'm fine, thanks. And you?","Long time no see","What's your name?"],
-	Mandarin: ["huānyíng guānglín", "nín hǎo","wǒ hěn hǎo nǐ ne","hǎojǐu bújiàn","nín guìxìng dàmíng"]
+	English: ["welcome","hello","I'm fine, thanks. And you?","Long time no see","What's your name?","My name is","Where are you from?","I'm from"."Pleased to meet you","Bon appetit"," don't understand"."Please say that again"],
+	Mandarin: ["huānyíng guānglín", "nín hǎo","wǒ hěn hǎo nǐ ne","hǎojǐu bújiàn","nín guìxìng dàmíng","wǒ jiào","nǐ shì nǎguórén","wǒ shì ... rén","wǒ hěn gāoxìng gěn nǐ jiànmiàn","qǐng màn yòng","wǒ tīngbùdǒng"."qǐng nǐ zài shuō yīcì hǎo ma"]
 };
 
 
